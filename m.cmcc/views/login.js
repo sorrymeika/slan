98cc7e47a1099288fd186bd3c1344f69a37e5c53
孙路 <sunlu@sunludeMacBook-Pro-4.local>
2016-10-25 01:05:27 +0800

    update

--- m.cmcc/views/login.js
+++ m.cmcc/views/login.js
@@ -10,18 +10,36 @@ var popup = require('widget/popup');
 var bridge = require('bridge');
 
 var aes = require('util/aes');
+var RSA = require('util/rsa');
+
+var rsa = new RSA();
 
 
-var key = aes.genKey();
+var res = rsa.setPrivateKey("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCczoLxjmckSX9oJBNSXY1rKWK5\
+urp3WzPiLhctBOahVSaSeWhVKif1Q1VSfDhnEw93pNRX6vxwsZ215x8iAFU/Xp8q\
+Pdj+S3mVjefkHZQRBohnJ1Bs7qGf+794yKOXyeUOUXqMbeIBuZu7MmZkNcnjLU8r\
+Y9SWbWtJmH6pjEYS5QIDAQAB")
+    .encrypt("asdfafsasdf我们都是白痴asdfasfaf");
 
-console.log(key);
+console.log(res);
 
-var en = aes.encrypt(key, '我们');
+rsa.setPrivateKey("MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJzOgvGOZyRJf2gk\n" +
+    "E1JdjWspYrm6undbM+IuFy0E5qFVJpJ5aFUqJ/VDVVJ8OGcTD3ek1Ffq/HCxnbXn\n" +
+    "HyIAVT9enyo92P5LeZWN5+QdlBEGiGcnUGzuoZ/7v3jIo5fJ5Q5Reoxt4gG5m7sy\n" +
+    "ZmQ1yeMtTytj1JZta0mYfqmMRhLlAgMBAAECgYBtYJdyC1TiuOnIKLHZmCwW/c1B\n" +
+    "pICA1PtB38iGvI5rJsw7C7i8/yTULd2MJd3p+B+n4y/DC8R+j0ZlJgxPWVAcRk6g\n" +
+    "ATUEgWR6w3+QNgUg6Y7sjixK50Fn3EuzeO1CkS7KMjtr1wCiSPJraqZ4LiDvVLSx\n" +
+    "XW0mxjdAm2eJefWtmQJBAMkXY0qi4uqp7Bzlb51acCY4Uz8JlNOrQUlp5jXTEj0H\n" +
+    "atBX9+Npl5ttdfmHaqvRX+IyvQXQZ3SYDM7ybpoYcBcCQQDHn42bS9kPbQtdIKyr\n" +
+    "65eh35KKVkWWqllJ5WTZ7k41Tqafn7IhDXrxV8l6UErb+40VkxhwjLBWGWPcVFki\n" +
+    "DVZjAkAhiYcDwZq6rFA2mYU95wFhPm+TydNKYftndQPx4hvelcgYqzMqeEfSojTU\n" +
+    "wuLleOvW28Nuns1CQsGQGhqcFItNAkAIbNV/Lm5U1ldacNyYzPKzZEo9CHMbbskh\n" +
+    "80qUYBhw64bgyc6s0nLT6M6aGPVv32gO9wxbA7UVjTDHxc+VPlbdAkEAjSNLv/9P\n" +
+    "nI6RPwVR7PGvVc8q4MxkllNEQKUjPqlwJCpqC9nUe9DK2E3WCbcgp14Q/IFj/S7U\n" +
+    "kSYYIKcoCLQR3A==");
 
-var de = aes.decrypt(key, en);
-
-console.log(en, 'de:', de);
-
+var res = rsa.decrypt("Y7rxqtfcQzAbQ+ZVNUASxRcui2NVvSdZo/Jprdxy6xLjcp2qF2WHovwYgixYAXhz7Yf+7t/EuIFfKZiE4AmJ4ITPwS9nTY8ZL+bCUXHeieNbMakvYs96r6DQHwwcCaSlix202Q4qN4nSsJ2IQAkCeAsciJgUfFV+Lp5Mh3pipx4=");
+console.log(res);
 
 module.exports = Activity.extend({
 
--- webresource/js/util/aes.js
+++ webresource/js/util/aes.js
@@ -5,12 +5,18 @@ var util = require('util');
 
 module.exports = {
     encrypt: function (secretKey, word) {
+
+        secretKey = CryptoJS.enc.Utf8.parse(secretKey);
+        word = CryptoJS.enc.Utf8.parse(word);
+
         var encrypted = CryptoJS.AES.encrypt(word, secretKey, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 });
 
         return encrypted.toString();
     },
 
     decrypt: function (secretKey, word) {
+        secretKey = CryptoJS.enc.Utf8.parse(secretKey);
+
         var decrypt = CryptoJS.AES.decrypt(word, secretKey, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 });
 
         return CryptoJS.enc.Utf8.stringify(decrypt).toString();
